e5ea6fdb4408b55b0cf75d3feb7dc17422370892
edraqs <edwin@raquin.eu>
2020-03-17 14:54:07 +0100

    Modified sandbox to scrap into denzel.json

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ const cors = require('cors');
 const express = require('express');
 const helmet = require('helmet');
 const {PORT} = require('./constants');
+const imdb = require('./imdb');
+const DENZEL_IMDB_ID = 'nm0000243';
+
 
 const app = express();
 
@@ -13,9 +16,18 @@ app.use(helmet());
 
 app.options('*', cors());
 
-app.get('/', (request, response) => {
-  response.send({'ack': true});
+var jsonMovies;
+
+async function getMovies (actor = DENZEL_IMDB_ID) {
+  const movies = await imdb(DENZEL_IMDB_ID);
+  jsonMovies = JSON.stringify(movies, null, 2);
+}
+
+getMovies();
+app.get('/movies/populate/' + DENZEL_IMDB_ID, (request, response) => {
+    response.send(jsonMovies);
 });
+
 
 app.listen(PORT);
 console.log(`📡 Running on port ${PORT}`);
--- server/sandbox.js
+++ server/sandbox.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-console, no-process-exit */
 const imdb = require('./imdb');
 const DENZEL_IMDB_ID = 'nm0000243';
-const METASCORE = 77;
+const METASCORE = 70;
 
 async function start (actor = DENZEL_IMDB_ID, metascore = METASCORE) {
   try {
@@ -10,9 +10,16 @@ async function start (actor = DENZEL_IMDB_ID, metascore = METASCORE) {
     const awesome = movies.filter(movie => movie.metascore >= metascore);
 
     console.log(`🍿 ${movies.length} movies found.`);
-    console.log(JSON.stringify(movies, null, 2));
+    const jsonMovies = JSON.stringify(movies, null, 2)
+    console.log(jsonMovies);
     console.log(`🥇 ${awesome.length} awesome movies found.`);
     console.log(JSON.stringify(awesome, null, 2));
+    var fs = require('fs');
+    fs.writeFileSync("denzel.json", jsonMovies, function(err) {
+    if (err) {
+        console.log(err);
+    }
+});
     process.exit(0);
   } catch (e) {
     console.error(e);
