10b569aa1e7669f9d0e8ee8ce181f586caf14b1c
edraqs <edwin@raquin.eu>
2020-03-24 16:45:37 +0100

    Added some graphQL code, doesn't work, big rework ahead

--- server/index.js
+++ server/index.js
@@ -1,5 +1,13 @@
 const cors = require('cors');
 const express = require('express');
+const expressGraphQL = require('express-graphql')
+const{
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLList
+} = require('graphql')
 const helmet = require('helmet');
 const {
     PORT
@@ -11,11 +19,42 @@ const DENZEL_IMDB_ID = 'nm0000243';
 
 const app = express();
 
+const MovieType = new GraphQLObjectType({
+    name :'Movie',
+    description: 'Informations regarding a movie',
+    fields: () => ({
+        link: { type: GraphQLString },
+        metascore: { type: GraphQLInt },
+        synopsis: { type: GraphQLString },
+        title: { type: GraphQLString },
+        year: { type: GraphQLInt }
+    })
+})
+
+const QueryType = new GraphQLObjectType({
+    name: 'Query',
+    fields: () => ({
+        movies: {
+            type: new GraphQLList(MovieType),
+            description: 'List of books',
+            resolve: () => movies
+        }
+    })
+})
+
+const schema = new GraphQLSchema({
+    query: QueryType
+})
+
 module.exports = app;
 
 app.use(require('body-parser').json());
 app.use(cors());
 app.use(helmet());
+app.use('/graphql',expressGraphQL({
+    schema: schema,
+    graphiql: true
+}))
 
 app.options('*', cors());
 const dbName = "denzel-webapp";
